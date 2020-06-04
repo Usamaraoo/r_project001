608515a2290a20d3ef9719f436846b2f6cfade8c
usama <42535017+Usamaraoo@users.noreply.github.com>
2020-06-04 17:56:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <br/>
+          Update by usama
+         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
